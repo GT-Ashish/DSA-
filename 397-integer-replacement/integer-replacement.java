a72a1d48c8f553f34a14a2d9a036dfeaebb09b76
GT-Ashish <murarkaashish6294@gmail.com>
2025-08-23 20:11:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (55.39%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int integerReplacement(long n) {
+        int count=0;
+        
+        while(n!=1){
+        if(n%2==0){
+            n=n/2;
+            count++;
+        }
+        else if((n-1)%4==0 ||n==3) {
+            n-=1;
+            count++;
+         }
+        else {
+            n+=1;
+            count++;
+        }
+        }
+        return count;
+        
+        
+    }
+}
