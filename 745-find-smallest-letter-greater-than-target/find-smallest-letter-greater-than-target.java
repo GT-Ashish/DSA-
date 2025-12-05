e2a72d5eaaf066ec164339903c3b246c04fabce5
GT-Ashish <murarkaashish6294@gmail.com>
2025-12-06 00:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (32.15%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -7,28 +7,26 @@ class Solution {
         while(start<=end){
             mid=start+(end-start)/2;
 
-            if(letters[mid]==target){
+            // if(letters[mid]==target){
                
-                while(mid<letters.length-1 && letters[mid]==letters[mid+1] ){
-                    mid+=1;
-                }
-                if(mid+1>letters.length-1 || letters[mid+1]<=target)
-                return letters[0];
-                return letters[mid+1];
-            }
-            else if(letters[mid]<target){
+            //     while(mid<letters.length-1 && letters[mid]==letters[mid+1] ){
+            //         mid+=1;
+            //     }
+            //     if(mid+1>letters.length-1 || letters[mid+1]<=target)
+            //     return letters[0];
+            //     return letters[mid+1];
+            // }
+            if(letters[mid]<=target){
                 start=mid+1;
             }
             else{
                 end=mid-1;
             }
         }
-        if(start>letters.length-1){
-            start=letters.length-1;
+        if(start>letters.length-1 || letters[start]<=target){
+           return letters[0];
         }
-        if(letters[start]<target){
-            return letters[0];
-        }
+        
         else{
             return letters[start];
         }
